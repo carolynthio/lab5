ef6a25f8a14490739b73936c766d1f46900a92cf
Carolyn Thio <carolyn.thio@yahoo.com>
2016-02-04 03:14:28 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,12 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	
+	$(".nameClass").click(function(e){
+		e.preventDefault();
+		var name = $(this).text();
+		$(this).text(anagrammedName(name));
+	})
 })
 
 /*
@@ -44,3 +50,4 @@ function anagrammedName(name) {
 		return name;
 	}
 }
+
